233eeaaa28669f39f2e399dfee9558bb8412e683
salomon Alfredo Mena Lazarte <salomon.mena@uab.edu.bo>
2023-10-09 14:16:58 -0400

    solucionando conflictos

--- resources/views/pages/modal/delete.blade.php
+++ resources/views/pages/modal/delete.blade.php
@@ -1,50 +1,49 @@
 <div class="modal fade" id="Modaldelete" tabindex="-1" role="dialog" aria-labelledby="ModalLabel">
-    <div class="modal-dialog" role="document">
-        <div class="modal-content">
-            <div class="modal-header">
-                <h4 class="modal-title" id="myModalLabel">Eliminar Publicación</h4>
-                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                    <span aria-hidden="true">&times;</span>
-                </button>
-            </div>
-            <div class="modal-body">
-                <p id="publicacionIdText"></p>
-                <p id="mensajeEliminar"></p>
-            </div>
-            <div class="modal-footer">
-                <button type="button" class="btn btn-secondary" data-dismiss="modal">Cerrar</button>
-                <button id="eliminarBtn" type="button" class="btn btn-success" data-publicacion-id="{{ $publicacion->id }}" onclick="eliminarPublicacion()">Eliminar</button>
-            </div>
-        </div>
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header bg-info">
+        <h4 class="modal-title" id="myModalLabel">Eliminar Publicacion</h4>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-footer d-flex justify-content-center">
+        <button type="button" class="btn btn-primary" data-dismiss="modal">Cancelar</button>
+        <div class="mx-4"></div>
+        <button type="button" class="btn btn-danger eliminar" data-publicacion-id="{{ $publicacion->id }}">Eliminar</button>
+      </div>
     </div>
+  </div>
 </div>
 
 <script>
-    
-    function eliminarPublicacion() {
-      var id = $('#eliminarBtn').data('publicacion-id')
-        console.log('id publicacion:', id);
-        // Enviar la solicitud DELETE para eliminar la publicación
-        fetch(`/eliminar-publicacion`, {
-            method: 'DELETE',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({ id: id })
-        })
-        .then(response => {
-            if (!response.ok) {
-                throw new Error('Network response was not ok');
-            }
-            return response.json();
-        })
-        .then(data => {
-            console.log('Publicación eliminada correctamente:', data.message);
-            $('#mensajeEliminar').text('Publicación eliminada correctamente').addClass('text-success').removeClass('text-danger');
-        })
-        .catch(error => {
-            console.error('Error al eliminar la publicación:', id);
-            $('#mensajeEliminar').text('Error al eliminar la publicación').addClass('text-danger').removeClass('text-success');
+    document.addEventListener('DOMContentLoaded', function() {
+        const eliminarBotones = document.querySelectorAll('.eliminar');
+
+        eliminarBotones.forEach(boton => {
+            boton.addEventListener('click', function() {
+                const publicacionId = this.dataset.publicacionId;
+
+                // if (confirm('¿Estás seguro de que quieres eliminar esta publicación?')) {
+                    fetch(`/eliminar-publicacion/${publicacionId}`, {
+                        method: 'DELETE',
+                        headers: {
+                            'X-CSRF-TOKEN': '{{ csrf_token() }}',
+                            'Content-Type': 'application/json'
+                        }
+                    })
+                    .then(response => response.json())
+                    .then(data => {
+                        if (data.success) {
+                            // alert(data.message);
+                            location.reload();
+                        } else {
+                            alert(data.message);
+                        }
+                    })
+                    .catch(error => console.error('Error:', error));
+                // }
+            });
         });
-    }
+    });
 </script>
--- resources/views/pages/modal/read.blade.php
+++ resources/views/pages/modal/read.blade.php
@@ -1,7 +1,7 @@
 <div class="modal fade" id="Modalread" tabindex="-1" role="dialog" aria-labelledby="ModalLabel">
   <div class="modal-dialog" role="document">
     <div class="modal-content">
-      <div class="modal-header">
+      <div class="modal-header bg-info">
         <h4 class="modal-title" id="myModalLabel">Ver Publicacion</h4>
         <button type="button" class="close" data-dismiss="modal" aria-label="Close">
           <span aria-hidden="true">&times;</span>
