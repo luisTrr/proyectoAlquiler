81f65bf77f752ec5f7ef4c23018d2b8ee21257c7
salomon Alfredo Mena Lazarte <salomon.mena@uab.edu.bo>
2023-11-19 16:40:44 -0400

    Se agrego la vista de la ubicacion en ver detalles de publicacion

--- resources/views/pages/ver.blade.php
+++ resources/views/pages/ver.blade.php
@@ -108,6 +108,9 @@
             <strong>Dirección:</strong>
             {{ $publicacion->direccion }}
           </div>
+          <div class="col-md-12">
+            <div id="mapa" style="width: 100%; height: 400px;"></div>
+          </div>
           <div class="form-group">
             <strong>Precio:</strong>
             {{ $publicacion->precio }}
@@ -323,6 +326,39 @@
     </div>
   </div>
 </div>
+<script>
+    function iniciarMapa(){
+      var latitud = {{ $publicacion->latitud }};
+      var longitud = {{ $publicacion->longitud }};
 
+      coordenadas = {
+        lng: longitud,
+        lat: latitud
+      }
+
+      generarMapa(coordenadas);
+    }
+
+    function generarMapa(coordenadas){
+      var mapa = new google.maps.Map(document.getElementById('mapa'),
+      {
+        zoom: 15,
+        center: new google.maps.LatLng(coordenadas.lat, coordenadas.lng)
+      });
+
+      marcador = new google.maps.Marker({
+        map: mapa,
+        draggable: false,
+        position: new google.maps.LatLng(coordenadas.lat, coordenadas.lng)
+      });
+
+      marcador.addListener('dragend',function(event){
+        document.getElementById("latitud").value = this.getPosition().lat();
+        document.getElementById("longitud").value = this.getPosition().lng();
+      })
+    }
+  </script>
+
+  <script src="https://maps.googleapis.com/maps/api/js?key=&callback=iniciarMapa"></script>
 </body>
 </html>
